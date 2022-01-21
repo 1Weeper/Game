776b0b702f43ed822514006d5e0540196acba950
Bob-Mcguire <80228122+Bob-Mcguire@users.noreply.github.com>
2022-01-21 22:45:05 +0200

    sex3

--- main.cpp
+++ main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
 #include <windows.h>
 
+void ClearScreen()
+  {
+  HANDLE                     hStdOut;
+  CONSOLE_SCREEN_BUFFER_INFO csbi;
+  DWORD                      count;
+  DWORD                      cellCount;
+  COORD                      homeCoords = { 0, 0 };
+
+  hStdOut = GetStdHandle( STD_OUTPUT_HANDLE );
+  if (hStdOut == INVALID_HANDLE_VALUE) return;
+
+  /* Get the number of cells in the current buffer */
+  if (!GetConsoleScreenBufferInfo( hStdOut, &csbi )) return;
+  cellCount = csbi.dwSize.X *csbi.dwSize.Y;
+
+  /* Fill the entire buffer with spaces */
+  if (!FillConsoleOutputCharacter(
+    hStdOut,
+    (TCHAR) ' ',
+    cellCount,
+    homeCoords,
+    &count
+    )) return;
+
+  /* Fill the entire buffer with the current colors and attributes */
+  if (!FillConsoleOutputAttribute(
+    hStdOut,
+    csbi.wAttributes,
+    cellCount,
+    homeCoords,
+    &count
+    )) return;
+
+  /* Move the cursor home */
+  SetConsoleCursorPosition( hStdOut, homeCoords );
+  }
+
 using namespace std;
 
 bool game_running = true;
@@ -39,12 +76,12 @@ int win()
 {
     cout << endl << "Congrats you past level 1!" << endl;
     game_running = false;
-    system("pause");
+    cin.get();
 }
 int lost()
 {
     cout << endl << "YOU DIED!" << endl;
-    system("pause");
+    cin.get();
 }
 
 
@@ -52,7 +89,7 @@ int main()
 {
     while(game_running == true)
     {
-        system("cls");
+        ClearScreen();
         for(int display=0; display<xres; display++)
         {
             cout << map[display] << endl;
